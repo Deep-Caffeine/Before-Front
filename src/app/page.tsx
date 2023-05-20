856835ca3f31264e38c066afdaf55f9371793391
dongwoochoi <dong99woo@gmail.com>
2023-05-20 15:30:30 +0900

    [Feat] 탈퇴를 위한 fetchDelete 함수 생성 후 버튼에 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import axios from 'axios';
+
 export default function Home() {
+
+  const fetchDelete = async () => {
+    try {
+      const response = await axios.get('');
+      console.log(response.data);
+      if (response.status === 200) {
+        alert('정상적으로 탈퇴 되었습니다.');
+      }
+      if (response.status === 401) {
+        alert('인증되지 않아 해당 작업을 수행할 수 없습니다.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main>
-      <button>Delete</button>
+      <button onClick={()=>{ 
+        fetchDelete();
+       }}>Delete</button>
     </main>
   )
 }
