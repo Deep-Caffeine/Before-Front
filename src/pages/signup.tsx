230ca5bbfe7f38047322484d52a0ffcfa3e9944e
Jinwoo <zaqaz113@naver.com>
2023-05-21 21:46:18 +0900

    [Feat] 상태코드에 따른 회원가입 에러 처리

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { useState, FormEvent, ChangeEvent } from "react";
 import { SignUp } from "../types/signup";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState<SignUp>({
     email: "",
     password: "",
@@ -25,16 +27,43 @@ export default function Signup() {
     try {
       const response = await axios.post(
         "http://www.ideaconnect.online/user",
-        formData,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
+        formData
       );
-      console.log(response);
+      // 상태 코드에 따른 처리
+      if (response.status === 200) {
+        // 성공적으로 처리된 경우
+        // 로그인 페이지로 이동
+        navigate("/login");
+      } else if (response.status === 400) {
+        // 잘못된 요청인 경우
+        // false 데이터 다시 작성해달라고 요청하기
+        const { data } = response;
+        if (data && typeof data === "object") {
+          // 서버에서 false 값을 받아옴
+          const { email, password, username, phone, birth } = data;
+          if (email === false) {
+            alert("이메일 형식이 올바르지 않습니다.");
+          }
+          if (password === false) {
+            alert("비밀번호를 8자 이상 작성해주세요");
+          }
+          if (username === false) {
+            alert("닉네임이 올바르지 않습니다.");
+          }
+          if (phone === false) {
+            alert("전화번호 형식이 올바르지 않습니다.")
+          }
+          if (birth === false) {
+            alert("생년월일 형식이 올바르지 않습니다.");
+          }
+        }
+      } else if (response.status === 500) {
+        // 서버 내부 오류인 경우
+        // 다시 회원가입 요청 하기
+        alert('다시 회원가입 요청을 해주시길 바랍니다.')
+      }
     } catch (error) {
-      console.error(error);
+        alert("회원가입 실패");
     }
   };
 
